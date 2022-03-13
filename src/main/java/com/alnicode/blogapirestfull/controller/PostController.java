b18f5ae06db09bc4eaab8457551deffe66c7c5fd
alnicode32 <albensnico@gmail.com>
2022-03-12 22:19:55 -0500

    Creación de controlador para los Posts y método para guardar un nuevo Post

---
+++ src/main/java/com/alnicode/blogapirestfull/controller/PostController.java
@@ -0,0 +1,25 @@
+package com.alnicode.blogapirestfull.controller;
+
+import com.alnicode.blogapirestfull.dto.PostDTO;
+import com.alnicode.blogapirestfull.service.PostService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+    
+    @Autowired
+    private PostService postService;
+
+    @PostMapping()
+    public ResponseEntity<PostDTO> savePost(@RequestBody PostDTO postDTO) {
+        return new ResponseEntity<>(this.postService.createPost(postDTO), HttpStatus.CREATED);
+    }
+}
