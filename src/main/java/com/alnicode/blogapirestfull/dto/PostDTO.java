9ca07cbff2fa26be82105ed6da33c5f7c019f226
alnicode32 <albensnico@gmail.com>
2022-03-12 21:57:45 -0500

    Añadido constructor con todos los argumentos

--- src/main/java/com/alnicode/blogapirestfull/dto/PostDTO.java
+++ src/main/java/com/alnicode/blogapirestfull/dto/PostDTO.java
@@ -1,10 +1,12 @@
 package com.alnicode.blogapirestfull.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class PostDTO {
