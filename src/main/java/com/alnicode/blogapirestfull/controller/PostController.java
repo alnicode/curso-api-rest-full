5c7341991cae763ff275bc762d4414613f4373b2
alnicode32 <albensnico@gmail.com>
2022-03-12 23:57:39 -0500

    Añadido método para eliminar en el controlador

--- src/main/java/com/alnicode/blogapirestfull/controller/PostController.java
+++ src/main/java/com/alnicode/blogapirestfull/controller/PostController.java
@@ -8,6 +8,7 @@ import com.alnicode.blogapirestfull.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,5 +43,11 @@ public class PostController {
     public ResponseEntity<PostDTO> updatePost(@RequestBody PostDTO postDTO, @PathVariable(name = "id") long idPost) {
         var post = this.postService.updatePost(postDTO, idPost);
         return new ResponseEntity<>(post, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePosting(@PathVariable(name = "id") long idPost) {
+        this.postService.deletePost(idPost);
+        return new ResponseEntity<>("Publicación eliminada con éxito", HttpStatus.OK);
     }
 }
