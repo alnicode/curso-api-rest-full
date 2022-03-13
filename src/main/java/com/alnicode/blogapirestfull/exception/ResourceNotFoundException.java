935dcc33aefc95ffc2e464d0ddd6866f0fc50b83
alnicode32 <albensnico@gmail.com>
2022-03-12 21:04:50 -0500

    Creación base de excepcion en caso que no encuentre un recurso solicitado

---
+++ src/main/java/com/alnicode/blogapirestfull/exception/ResourceNotFoundException.java
@@ -0,0 +1,23 @@
+package com.alnicode.blogapirestfull.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    private String resourceName;
+    private String fieldName;
+    private long fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue) {
+        super(resourceName + " No encontrado con: " + fieldName + ", '" + fieldValue + "'.");
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+}
