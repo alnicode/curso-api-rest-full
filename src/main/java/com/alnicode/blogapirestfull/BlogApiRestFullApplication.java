0b29519c2d661a9c70e75e4154d4601732b55d3c
alnicode32 <albensnico@gmail.com>
2022-03-12 20:16:16 -0500

    Inicio del curso.

---
+++ src/main/java/com/alnicode/blogapirestfull/BlogApiRestFullApplication.java
@@ -0,0 +1,13 @@
+package com.alnicode.blogapirestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApiRestFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alnicode/blogapirestfull/BlogApiRestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alnicode.blogapirestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiRestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
