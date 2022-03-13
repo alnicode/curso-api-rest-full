789402c1f9241c6bab474c006d0c2f133490f97a
alnicode32 <albensnico@gmail.com>
2022-03-12 22:50:25 -0500

    Creado método en el controlador para obtener todos los posts

--- src/main/java/com/alnicode/blogapirestfull/controller/PostController.java
+++ src/main/java/com/alnicode/blogapirestfull/controller/PostController.java
@@ -1,11 +1,14 @@
 package com.alnicode.blogapirestfull.controller;
 
+import java.util.List;
+
 import com.alnicode.blogapirestfull.dto.PostDTO;
 import com.alnicode.blogapirestfull.service.PostService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +24,10 @@ public class PostController {
     @PostMapping()
     public ResponseEntity<PostDTO> savePost(@RequestBody PostDTO postDTO) {
         return new ResponseEntity<>(this.postService.createPost(postDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping()
+    public List<PostDTO> getAllPosts() {
+        return this.postService.getAllPosts();
     }
 }
