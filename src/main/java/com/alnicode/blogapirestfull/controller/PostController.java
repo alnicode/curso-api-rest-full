2b3d381bd2111dec8803cdc2dcf8e61009075982
alnicode32 <albensnico@gmail.com>
2022-03-12 23:17:23 -0500

    Definido método en el controlador para obtener un post según el id

--- src/main/java/com/alnicode/blogapirestfull/controller/PostController.java
+++ src/main/java/com/alnicode/blogapirestfull/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +30,10 @@ public class PostController {
     @GetMapping()
     public List<PostDTO> getAllPosts() {
         return this.postService.getAllPosts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDTO> getPost(@PathVariable(name = "id") long idPost) {
+        return ResponseEntity.ok(this.postService.getPost(idPost));
     }
 }
