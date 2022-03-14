3a9f25f4245ff8acada968301ddeaec5234f43f5
alnicode32 <albensnico@gmail.com>
2022-03-13 20:45:39 -0500

    Añadido constructor con todos los parámetros

--- src/main/java/com/alnicode/blogapirestfull/dto/PostResponse.java
+++ src/main/java/com/alnicode/blogapirestfull/dto/PostResponse.java
@@ -2,11 +2,13 @@ package com.alnicode.blogapirestfull.dto;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class PostResponse {
