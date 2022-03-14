15942e2cd43ad5dc77e912c6eb791b91afb1e449
alnicode32 <albensnico@gmail.com>
2022-03-14 18:26:07 -0500

    Ahora el controlador puede listar los comentarios de un post

--- src/main/java/com/alnicode/blogapirestfull/controller/CommentController.java
+++ src/main/java/com/alnicode/blogapirestfull/controller/CommentController.java
@@ -1,11 +1,14 @@
 package com.alnicode.blogapirestfull.controller;
 
+import java.util.List;
+
 import com.alnicode.blogapirestfull.dto.CommentDTO;
 import com.alnicode.blogapirestfull.service.CommentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommentController {
     @Autowired
     private CommentService commentService;
+
+    @GetMapping("/posts/{postId}/comments")
+    public List<CommentDTO> getCommentsByPostId(@PathVariable(value = "postId") long idPost) {
+        return this.commentService.getByIdPost(idPost);
+    }
 
     @PostMapping("/posts/{postId}/comments")
     public ResponseEntity<CommentDTO> postComment(@PathVariable(value = "postId") long idPost, @RequestBody CommentDTO commentDTO) {
