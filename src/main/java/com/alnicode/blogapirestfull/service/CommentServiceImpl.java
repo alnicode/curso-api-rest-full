2c74d83e25bd250495b7f3e913b4e4d9b2ef4125
alnicode32 <albensnico@gmail.com>
2022-03-13 22:15:13 -0500

    Inyectadas las dependencias del repositorio del proyecto

--- src/main/java/com/alnicode/blogapirestfull/service/CommentServiceImpl.java
+++ src/main/java/com/alnicode/blogapirestfull/service/CommentServiceImpl.java
@@ -1,11 +1,19 @@
 package com.alnicode.blogapirestfull.service;
 
 import com.alnicode.blogapirestfull.dto.CommentDTO;
+import com.alnicode.blogapirestfull.repository.CommentRepository;
+import com.alnicode.blogapirestfull.repository.PostRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentServiceImpl implements CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private PostRepository postRepository;
 
     @Override
     public CommentDTO createComment(long idPost, CommentDTO commentDTO) {
