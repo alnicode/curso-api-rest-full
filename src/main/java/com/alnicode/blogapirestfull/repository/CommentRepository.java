dd4619bb857699f13b1e72f244767b6e7069de7e
alnicode32 <albensnico@gmail.com>
2022-03-14 18:21:41 -0500

    Creado método para obtener comentarios según el id del post

--- src/main/java/com/alnicode/blogapirestfull/repository/CommentRepository.java
+++ src/main/java/com/alnicode/blogapirestfull/repository/CommentRepository.java
@@ -1,9 +1,11 @@
 package com.alnicode.blogapirestfull.repository;
 
+import java.util.List;
+
 import com.alnicode.blogapirestfull.entity.Comment;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface CommentRepository extends CrudRepository<Comment, Long> {
-    
+    List<Comment> findByPostIdPost(long idPost);
 }
