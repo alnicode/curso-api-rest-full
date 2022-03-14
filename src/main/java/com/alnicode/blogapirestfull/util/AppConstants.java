b49318734b1f380357416bc82c212098810fc24a
alnicode32 <albensnico@gmail.com>
2022-03-13 21:29:32 -0500

    Creación de interface con constantes

---
+++ src/main/java/com/alnicode/blogapirestfull/util/AppConstants.java
@@ -0,0 +1,8 @@
+package com.alnicode.blogapirestfull.util;
+
+public interface AppConstants {
+    String DEFAULT_PAGE_NUMBER = "0";
+    String DEFAULT_PAGE_SIZE = "10";
+    String DEFAULT_SORT_BY = "idPost";
+    String DEFAULT_SORT_DIR = "asc";
+}
